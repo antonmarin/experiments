c3d95032f35de824eb811467b8d02d80b1bb6764
Anton Marin <antonaryo85@gmail.com>
2020-12-17 21:05:02 +0300

    base

---
+++ php-symfony-cli/src/entrypoints/TestDebugCli.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace antonmarin\experiment\entrypoints;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TestDebugCli extends Command
+{
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    protected function configure(): void
+    {
+        $this
+            ->setName('test:logger-debug')
+            // the short description shown while running "php bin/console list"
+            ->setDescription('Test command');
+    }
+
+    /** @noinspection PhpMissingParentCallCommonInspection */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        return Command::SUCCESS;
+    }
+}
